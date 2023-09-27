79557f8aab1d1c9c6424539e2f128edf4aaee38a
Camilo Gálvez Vidal <c.galvezv@uniandes.edu.co>
2023-09-26 22:22:01 -0300

    FEAT: update ips

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "54.160.242.176"]
+ALLOWED_HOSTS = ["localhost", "34.230.76.67"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "35.172.225.221",  # Dirección IP de la base de datos
+        "HOST": "54.197.170.33",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "3.83.237.33"
+MQTT_HOST = "54.235.12.227"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
